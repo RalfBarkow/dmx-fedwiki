65ee3cbc297d0bcae8cfaa9048e2464755c3b552
Ralf Barkow <ralf.barkow@me.com>
2025-06-13 11:32:38 +0200

    Initial commit of dmx-fedwiki module

---
+++ src/main/java/ch/dreyeck/fedwiki/FedWikiPlugin.java
@@ -0,0 +1,16 @@
+package ch.dreyeck.fedwiki;
+
+import systems.dmx.core.osgi.PluginActivator;
+
+import java.util.logging.Logger;
+
+public class FedWikiPlugin extends PluginActivator {
+
+    @Override
+    public void init() {
+        Logger logger = null;
+        logger.info("### dmx-fedwiki plugin initialized ###");
+
+        // Example: register a service or command here
+    }
+}
